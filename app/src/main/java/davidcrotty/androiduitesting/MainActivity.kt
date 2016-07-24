8bf7bb426446980cba6644ba4f5516895e85dfc0
David Crotty <david.crotty.work@gmail.com>
2016-07-24 19:23:41 +0100

    Adds Kotlin 1.3 Adds Main Activity.

---
+++ app/src/main/java/davidcrotty/androiduitesting/MainActivity.kt
@@ -0,0 +1,14 @@
+package davidcrotty.androiduitesting
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by DavidHome on 24/07/2016.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
