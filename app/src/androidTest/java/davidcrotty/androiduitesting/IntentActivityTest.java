eb8b9e1abd4a2a1d9b604942ffe91d0c957cbbc7
David Crotty <david.crotty.work@gmail.com>
2016-09-16 13:45:36 +0100

    Intent Activity to spec of API 24 testing. Allows passing of intent per test.

--- app/src/androidTest/java/davidcrotty/androiduitesting/IntentActivityTest.java
+++ app/src/androidTest/java/davidcrotty/androiduitesting/IntentActivityTest.java
@@ -3,11 +3,13 @@ package davidcrotty.androiduitesting;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,21 +22,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * Created by DavidHome on 24/08/2016.
  */
 @RunWith(AndroidJUnit4.class)
-//FIXME ActivityInstrumentationTestCase2 deprecated in N, see https://developer.android.com/reference/android/test/ActivityInstrumentationTestCase2.html
-public class IntentActivityTest extends ActivityInstrumentationTestCase2<IntentActivity> {
-    public IntentActivityTest() {
-        super(IntentActivity.class);
-    }
+public class IntentActivityTest {
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        Intent intent = new Intent();
-        intent.putExtra("TEXT_KEY", "hello");
-        setActivityIntent(intent);
-        getActivity();
-    }
+    @Rule
+    public ActivityTestRule<IntentActivity> activityRule =
+            new ActivityTestRule<>(IntentActivity.class);
+
 
     @Test
     public void test_when_not_passing_intent() {
@@ -44,6 +37,10 @@ public class IntentActivityTest extends ActivityInstrumentationTestCase2<IntentA
 
     @Test
     public void test_when_passing_intent() {
+        Intent intent = new Intent();
+        intent.putExtra("TEXT_KEY", "hello");
+        activityRule.launchActivity(intent);
+
         onView(withId(R.id.text_view))
         .check(matches(withText("hello")));
     }
